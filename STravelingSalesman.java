e29da64e43b4037a9f2bf04dd889a372d39b4ad9
dfradkin2016 <dfradkin2016@pingry.org>
2016-02-26 08:39:52 -0500

    FinalCommit
    Added comments

--- STravelingSalesman.java
+++ STravelingSalesman.java
@@ -1,13 +1,26 @@
-//Daria Fradkin
-//February 11, 2016
+/**
+This is a "stupid" traveling salesman.
+It creates a bunch of points with random distances between them.
+The distances between points are not related.
+Starting from the first point, it chooses the shortest distance to a point it has not visited yet.
+This way, it goes to every point in a relatively quick path, although not the fastest.
+It does not return to where it started from.
 
+@author	Daria Fradkin
+@version	February 17, 2016
+*/
 import java.util.ArrayList;
 import java.lang.Math;
 
 public class STravelingSalesman
 {
+	//All points within the "map", all vertexes
 	private static ArrayList<Vertex<String>> all;
 	
+	/**
+	Initializes the list of vertexes.
+	Calls join to connect the points.
+	*/
 	private static void init()
 	{
 		all = new ArrayList<Vertex<String>>();
@@ -30,17 +43,30 @@ public class STravelingSalesman
 		join();
 	}
 	
+	/**
+	Joins the points with random lengths.
+	*/
 	private static void join()
 	{
 		for (int i = 0; i < all.size(); i++)
 		{
+			//only need to connect with what comes afterwards
+			//because every pair only needs to be called once because
+			//connect connects forwards and backwards
 			for (int j = i + 1; j < all.size(); j++)
 			{
+				//Connect is from vertex class
+				//Connects them forwards and backwards.
 				all.get(i).connect(all.get(j), (int) (Math.random()*100));
 			}
 		}
 	}
 	
+	/**
+	Checks if all points have been visited.
+	
+	@return	true if all visited
+	*/
 	private static boolean allVisited()
 	{
 		for (int i = 0; i < all.size(); i++)
@@ -51,19 +77,28 @@ public class STravelingSalesman
 		return true;
 	}
 	
+	/**
+	Method with main algorithm
+	*/
 	private static void traverse()
 	{
-		//System.out.println("h");
+		//starts at first point
 		Vertex<String> current = all.get(0);
+		//keeps track of best path
 		int best = 0;
+		//stops when all points visited
 		while (!allVisited())
 		{
+			//Print is to see that it is running correctly
 			print(current);
+			//best starts at nonexistent path
 			best = all.size() + 1;
 			for (int i = 0; i < current.pointers().size(); i++)
 			{
 				if (!current.pointers().get(i).beenVisited())
 				{
+					//if its the first unvisited point
+					//cant call get(best) because index out of bounds
 					if (best == all.size() + 1)
 					{
 						best = i;
@@ -78,17 +113,24 @@ public class STravelingSalesman
 				}
 			}
 			current.setVisited(true);
+			//if there was a path that was not visited
+			//only not called when on last iteration
 			if (! (best == all.size() + 1))
 				current = current.pointers().get(best);
 		}
 	}
 	
+	/**
+	Print method.
+	Prints out where you are and what you are options with their lengths
+	*/
 	private static void print(Vertex<String> current)
 	{
 		System.out.println("You are currently in " + current.value());
 		System.out.println("Your options are:");
 		for (int i = 0; i < current.pointers().size(); i++)
 		{
+			//only prints if it is not visited yet
 			if (!current.pointers().get(i).beenVisited())
 			{
 				System.out.println(current.pointers().get(i).value() + current.costs().get(i));
@@ -96,6 +138,10 @@ public class STravelingSalesman
 		}
 	}
 	
+	/**
+	Main method.
+	Calls other methods.
+	*/
 	public static void main(String[] Args)
 	{
 		init();
--- Vertex.java
+++ Vertex.java
@@ -1,5 +1,13 @@
-//February 10, 2016
-//Daria Fradkin
+/**
+Vertex class.
+Part of a graph.
+Each vertex holds two ArrayLists - one with all the pointers it connects to,
+and one with their lengths.
+Only holds lengths as integers.
+
+@author	Daria Fradkin
+@version	February 10, 2016
+*/
 
 import java.util.ArrayList;
 
@@ -10,8 +18,11 @@ public class Vertex<E>
 	private ArrayList<Integer> costs;
 	private boolean beenVisited;
 	
-	/*
+	/**
 	Constructor.
+	Simple, just a value.
+	
+	@param	v Value
 	*/
 	public Vertex(E v)
 	{
@@ -22,52 +33,105 @@ public class Vertex<E>
 	}
 	
 	//2 way
+	/**
+	Connector
+	Connects forwards and backwards with the same length
+	
+	@param	v	other Vertex
+	@param	c	cost
+	*/
 	public void connect(Vertex v, int c)
 	{
+		//could also call this.add(v, c) for next two lines
 		pointers.add(v);
 		costs.add(c);
+		//needs to use add because pointers and costs are private in other Vertex
+		//(if there was no accessor (but there is))
 		v.add(this, c);
 	}
 	
-	//1 way
-	//from this vertex to another, but not back
-	//1-directional
+	/**
+	1-way connector
+	From this vertex to another, but not back
+	
+	@param	v	other Vertex
+	@param	c	cost
+	*/
 	public void add(Vertex v, Integer c)
 	{
 		pointers.add(v);
 		costs.add(c);
 	}
 	
+	/**
+	Value accessor
+	
+	@return	Value
+	*/
 	public E value()
 	{
 		return value;
 	}
 	
+	/**
+	Value modifier
+	
+	@param	v	Value
+	*/
 	public void setValue(E v)
 	{
 		value = v;
 	}
 	
+	/**
+	Pointers accessor.
+	Unnecessary method
+	
+	@return	pointers list
+	*/
 	public ArrayList<Vertex<E>> pointers()
 	{
 		return pointers;
 	}
 	
+	/**
+	Costs accessor.
+	Unnecessary method
+	
+	@return	costs list
+	*/
 	public ArrayList<Integer> costs()
 	{
 		return costs;
 	}
 	
+	/**
+	Checks if the point has been visited.
+	
+	@return	true if been visited
+	*/
 	public boolean beenVisited()
 	{
 		return beenVisited;
 	}
 	
+	/**
+	beenVisited modifier
+	sets beenVisited to boolean
+	
+	@param	b	beenVisited
+	*/
 	public void setVisited(boolean b)
 	{
 		beenVisited = b;
 	}
 	
+	/**
+	beenVisited modifier
+	Sets to the opposite of what it is now
+	
+	@param	b	beenVisited
+	*/
 	public void changeVisited(boolean b)
 	{
 		beenVisited = !beenVisited;
